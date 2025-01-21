e7a30f49f5c41152831801b95eedb713e20908c9
amania-jailbreak <amania.apple@icloud.com>
2025-01-21 10:50:05 +0900

    APIチェックタスクを追加し、定期的にメッセージをブロードキャストする機能を実装

--- src/main/java/jp/amania/abyss/abyss_plugin/Abyss_plugin.java
+++ src/main/java/jp/amania/abyss/abyss_plugin/Abyss_plugin.java
@@ -13,15 +13,24 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.scheduler.BukkitRunnable;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 
 public final class Abyss_plugin extends JavaPlugin implements Listener {
 
+    // Startup
     @Override
     public void onEnable() {
-        // Plugin startup logic
         Bukkit.getLogger().info("¥n AbyssPlugin Loaded!!¥n--------------------------¥nAuthor:amania¥nVersion:1.1.0");
         getServer().getPluginManager().registerEvents(this, this);
+        startApiCheckTask();
     }
+
+    // Events
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
@@ -41,5 +50,33 @@ public final class Abyss_plugin extends JavaPlugin implements Listener {
         player.sendMessage(ChatColor.RED + "基本的な禁止事故(チート、荒らし、その他マナーに反する行為)　などが確認された場合それに相応する処置を取る場合があります。");
         player.sendTitle("Welcome To amaniaServer","ようこそ！" , 0, 0, 0);
         player.playSound(player, Sound.ENTITY_PLAYER_LEVELUP, 0, 0);
+    }
+
+    // Functions
+
+    public void startApiCheckTask() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = URI.create("https://auth.amania.jp/minecraft/notice").toURL();
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String inputLine;
+                    StringBuilder content = new StringBuilder();
+                    while ((inputLine = in.readLine()) != null) {
+                        content.append(inputLine);
+                    }
+                    in.close();
+                    conn.disconnect();
+
+                    String message = content.toString();
+                    Bukkit.broadcastMessage(ChatColor.YELLOW + message);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.runTaskTimer(this, 0L, 1200L); 
     }
 }
